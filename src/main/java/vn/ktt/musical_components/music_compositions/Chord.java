86e902a8df9c9e6a0176dba8669b43b29a317a3f
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-11 11:30:10 +0700

    feat: refactor Chord related object value

--- src/main/java/vn/ktt/musical_components/music_compositions/Chord.java
+++ src/main/java/vn/ktt/musical_components/music_compositions/Chord.java
@@ -21,4 +21,26 @@ public class Chord {
     public String toString() {
         return rootKey.toString() + quality.getStringNotation();
     }
+
+    public enum Quality {
+        MAJOR(new int[]{0, 4, 7}),
+        MINOR(new int[]{0, 3, 7});
+
+        private final int[] intervals;
+
+        Quality(int[] intervals) {
+            this.intervals = intervals;
+        }
+
+        public int[] getIntervals() {
+            return intervals;
+        }
+
+        public String getStringNotation() {
+            if (this == Quality.MINOR) {
+                return "m";
+            }
+            return "";
+        }
+    }
 }
