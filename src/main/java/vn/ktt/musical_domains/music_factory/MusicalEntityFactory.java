f5bf89a01c1dc62b33d8c2b6900d90c0544b6ca1
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-12 01:41:34 +0700

    feat: implemented method to easily get Pitch

--- src/main/java/vn/ktt/musical_domains/music_factory/MusicalEntityFactory.java
+++ src/main/java/vn/ktt/musical_domains/music_factory/MusicalEntityFactory.java
@@ -1,22 +1,17 @@
 package vn.ktt.musical_domains.music_factory;
 
-import vn.ktt.musical_domains.music_compositions.Chord;
-import vn.ktt.musical_domains.music_compositions.Interval;
 import vn.ktt.musical_domains.music_elements.Pitch;
-import vn.ktt.musical_domains.music_services.MusicalValidator;
 
 public class MusicalEntityFactory {
-    private final MusicalValidator validator;
-    public MusicalEntityFactory(MusicalValidator validator) {
-        this.validator = validator;
-    }
+
     public Pitch getPitch(String noteNotation) {
-        return null;
-    }
-    public Chord getChord(String chordNotation) {
-        return null;
-    }
-    public Interval getInterval(String intervalNotation) {
-        return null;
+        if (Pitch.isNotValid(noteNotation)) {
+            throw new IllegalArgumentException("Unknown note with notation " + noteNotation);
+        }
+        return new Pitch(
+                Pitch.extractNote(noteNotation),
+                Pitch.extractAccidental(noteNotation),
+                Pitch.extractOctave(noteNotation)
+        );
     }
 }
