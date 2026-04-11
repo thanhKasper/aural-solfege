8503fe06c25e44a4d203528e266d2c91d73e491d
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-11 23:30:46 +0700

    feat: update main to test implementation

--- src/main/java/vn/ktt/Main.java
+++ src/main/java/vn/ktt/Main.java
@@ -1,6 +1,11 @@
 package vn.ktt;
 
+import vn.ktt.musical_domains.music_elements.Pitch;
+import vn.ktt.sound_controller.MidiSoundPlayer;
+
 public class Main {
     static void main() {
+        var soundPlayer = new MidiSoundPlayer();
+        var cMiddle = new Pitch(Pitch.Note.C, Pitch.Accidental.NONE, Pitch.Octave.FOURTH);
     }
 }
