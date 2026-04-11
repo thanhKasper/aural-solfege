c0272d4aeff77252a34f42df7f44f6bb3eb07afc
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-11 11:22:54 +0700

    feat: refactor related enum of Pitch

--- src/main/java/vn/ktt/Main.java
+++ src/main/java/vn/ktt/Main.java
@@ -1,14 +1,12 @@
 package vn.ktt;
 
-import vn.ktt.musical_components.music_elements.Accidental;
-import vn.ktt.musical_components.music_elements.Note;
-import vn.ktt.musical_components.music_elements.Octave;
+import vn.ktt.musical_components.music_elements.Pitch;
 import vn.ktt.musical_components.sound_generator.ResourceBasedSoundRepository;
 
 public class Main {
     static void main() {
         ResourceBasedSoundRepository repo = new ResourceBasedSoundRepository();
-        var result = repo.getSound(Note.C, Accidental.NONE, Octave.FOURTH);
+        var result = repo.getSound(Pitch.Note.C, Pitch.Accidental.NONE, Pitch.Octave.FOURTH);
         result.makeSound();
     }
 }
--- src/main/java/vn/ktt/musical_components/sound_generator/ISoundRepository.java
+++ src/main/java/vn/ktt/musical_components/sound_generator/ISoundRepository.java
@@ -1,10 +1,7 @@
 package vn.ktt.musical_components.sound_generator;
 
-import vn.ktt.musical_components.music_elements.Accidental;
-import vn.ktt.musical_components.music_elements.Note;
-import vn.ktt.musical_components.music_elements.Octave;
 import vn.ktt.musical_components.music_elements.Pitch;
 
 public interface ISoundRepository {
-    public Pitch getSound(Note note, Accidental accidental, Octave octave);
+    public Pitch getSound(Pitch.Note note, Pitch.Accidental accidental, Pitch.Octave octave);
 }
