da732e5acb5cad332efb811595f709bab5cc9a3a
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-11 16:57:28 +0700

    feat: update Pitch related repository

--- src/main/java/vn/ktt/musical_components/sound_generator/ISoundRepository.java
+++ src/main/java/vn/ktt/musical_components/sound_generator/ISoundRepository.java
@@ -1,7 +1,8 @@
 package vn.ktt.musical_components.sound_generator;
 
 import vn.ktt.musical_components.music_elements.Pitch;
+import vn.ktt.musical_components.music_elements.Pitch.*;
 
 public interface ISoundRepository {
-    public Pitch getSound(Pitch.Note note, Pitch.Accidental accidental, Pitch.Octave octave);
+    Pitch getPitch(Note note, Accidental accidental, Octave octave);
 }
---
+++ src/main/java/vn/ktt/musical_components/sound_generator/MidiSoundRepository.java
@@ -0,0 +1,4 @@
+package vn.ktt.musical_components.sound_generator;
+
+public class MidiSoundRepository {
+}
