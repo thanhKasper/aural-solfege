aa638e0415f71593c0abf9d9db07f02730b55e2e
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-11 17:08:50 +0700

    feat: add few more methods for ISoundRepository

--- src/main/java/vn/ktt/musical_components/sound_generator/ISoundRepository.java
+++ src/main/java/vn/ktt/musical_components/sound_generator/ISoundRepository.java
@@ -1,8 +1,11 @@
 package vn.ktt.musical_components.sound_generator;
 
+import vn.ktt.musical_components.music_compositions.Chord;
+import vn.ktt.musical_components.music_compositions.Interval;
 import vn.ktt.musical_components.music_elements.Pitch;
-import vn.ktt.musical_components.music_elements.Pitch.*;
 
 public interface ISoundRepository {
-    Pitch getPitch(Note note, Accidental accidental, Octave octave);
+    Pitch getPitch(String noteNotation);
+    Chord getChord(String chordNotation);
+    Interval getInterval(String intervalNotation);
 }
