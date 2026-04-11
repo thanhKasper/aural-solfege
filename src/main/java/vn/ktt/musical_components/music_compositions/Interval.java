58072b28cda79110885258cbd91618896ff3ae41
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-11 11:29:55 +0700

    feat: refactor Interval related Object Value

--- src/main/java/vn/ktt/musical_components/music_compositions/Interval.java
+++ src/main/java/vn/ktt/musical_components/music_compositions/Interval.java
@@ -19,4 +19,31 @@ public class Interval {
     }
 
     public void makeDownwardSound() {}
+
+    public enum IntervalNumber {
+        UNISON(0),
+        MINOR_2ND(1),
+        MAJOR_2ND(2),
+        MINOR_3RD(3),
+        MAJOR_3RD(4),
+        PERFECT_4TH(5),
+        AUGMENTED_4TH(5),
+        DIMINISHED_5TH(6),
+        PERFECT_5TH(7),
+        MINOR_6TH(8),
+        MAJOR_6TH(9),
+        MINOR_7TH(10),
+        MAJOR_7TH(11),
+        PERFECT_OCTAVE(12);
+
+        private final int halfSteps;
+
+        IntervalNumber(int halfSteps) {
+            this.halfSteps = halfSteps;
+        }
+
+        public int getHalfSteps() {
+            return halfSteps;
+        }
+    }
 }
