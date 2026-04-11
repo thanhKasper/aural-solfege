26ea978e60043b6bb65913ee7eac5ea4c11d1679
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-11 11:36:43 +0700

    feat: remove deprecated implementation

--- src/main/java/vn/ktt/Main.java
+++ src/main/java/vn/ktt/Main.java
@@ -1,12 +1,6 @@
 package vn.ktt;
 
-import vn.ktt.musical_components.music_elements.Pitch;
-import vn.ktt.musical_components.sound_generator.ResourceBasedSoundRepository;
-
 public class Main {
     static void main() {
-        ResourceBasedSoundRepository repo = new ResourceBasedSoundRepository();
-        var result = repo.getSound(Pitch.Note.C, Pitch.Accidental.NONE, Pitch.Octave.FOURTH);
-        result.makeSound();
     }
 }
--- src/main/java/vn/ktt/shared/sound_player/ISoundPlayer.java
+++ src/main/java/vn/ktt/shared/sound_player/ISoundPlayer.java
@@ -1,6 +1,11 @@
 package vn.ktt.shared.sound_player;
 
-public interface ISoundPlayer {
-    void play();
+import vn.ktt.musical_components.music_compositions.Chord;
+import vn.ktt.musical_components.music_compositions.Interval;
+import vn.ktt.musical_components.music_elements.Pitch;
 
+public interface ISoundPlayer {
+    void playPitch(Pitch pitch);
+    void playTriad(Chord chord);
+    void playInterval(Interval interval);
 }
