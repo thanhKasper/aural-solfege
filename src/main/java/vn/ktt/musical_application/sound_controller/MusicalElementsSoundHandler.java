2d0596b36738e3741b21003ea05111a0b37b7818
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-12 20:18:44 +0700

    feat: implement the IMusicalElementSoundHandler interface

--- src/main/java/vn/ktt/musical_application/sound_controller/MusicalElementsSoundHandler.java
+++ src/main/java/vn/ktt/musical_application/sound_controller/MusicalElementsSoundHandler.java
@@ -1,4 +1,29 @@
 package vn.ktt.musical_application.sound_controller;
 
-public class MusicalElementsSoundHandler {
+import vn.ktt.musical_application.sound_controller.inbound.IMusicalElementsSoundHandler;
+import vn.ktt.musical_application.sound_controller.outbound.ISoundPlayer;
+import vn.ktt.musical_domains.music_factory.IMusicalEntityFactory;
+
+public class MusicalElementsSoundHandler implements IMusicalElementsSoundHandler {
+    private final ISoundPlayer soundPlayer;
+    private final IMusicalEntityFactory musicalElementFactory;
+    public MusicalElementsSoundHandler(ISoundPlayer soundPlayer, IMusicalEntityFactory factory) {
+        this.soundPlayer = soundPlayer;
+        this.musicalElementFactory = factory;
+    }
+
+
+    @Override
+    public void playStackedInterval(String interval) {
+        var selectedInterval = this.musicalElementFactory.getInterval(interval);
+    }
+
+    @Override
+    public void playBrokenInterval(String interval, String anchoredPitch, boolean isUpward) {
+        var selectedInterval = this.musicalElementFactory.getInterval(interval);
+        var startPitch = this.musicalElementFactory.getPitch(anchoredPitch);
+        var endPitch = isUpward ? selectedInterval.upwardPitch(startPitch) : selectedInterval.downwardPitch(startPitch);
+        this.soundPlayer.playSingleSound(startPitch.toNotation());
+        this.soundPlayer.playSingleSound(endPitch.toNotation());
+    }
 }
