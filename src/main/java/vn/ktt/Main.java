824b9f557338b18319f6b5ceda26784f5584a1cf
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-12 10:36:33 +0700

    feat: update the main method to test

--- src/main/java/vn/ktt/Main.java
+++ src/main/java/vn/ktt/Main.java
@@ -1,11 +1,14 @@
 package vn.ktt;
 
-import vn.ktt.musical_domains.music_elements.Pitch;
-import vn.ktt.sound_controller.MidiSoundPlayer;
+import vn.ktt.musical_domains.music_factory.MusicalEntityFactory;
+import vn.ktt.musical_infrastructure.MidiSoundPlayer;
 
 public class Main {
     static void main() {
         var soundPlayer = new MidiSoundPlayer();
-        var cMiddle = new Pitch(Pitch.Note.C, Pitch.Accidental.NONE, Pitch.Octave.FOURTH);
+        var musicFactory = new MusicalEntityFactory();
+        soundPlayer.playPitch(musicFactory.getPitch("C4"));
+        soundPlayer.playPitch(musicFactory.getPitch("D4"));
+        soundPlayer.playPitch(musicFactory.getPitch("E4"));
     }
 }
