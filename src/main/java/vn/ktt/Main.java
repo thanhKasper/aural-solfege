8b3a090dcfa80139f70d46771eae615feebdd7df
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-04-12 13:52:25 +0700

    feat: update latest implementation

--- src/main/java/vn/ktt/Main.java
+++ src/main/java/vn/ktt/Main.java
@@ -5,10 +5,18 @@ import vn.ktt.musical_infrastructure.MidiSoundPlayer;
 
 public class Main {
     static void main() {
-        var soundPlayer = new MidiSoundPlayer();
         var musicFactory = new MusicalEntityFactory();
-        soundPlayer.playPitch(musicFactory.getPitch("C4"));
-        soundPlayer.playPitch(musicFactory.getPitch("D4"));
-        soundPlayer.playPitch(musicFactory.getPitch("E4"));
+        var soundPlayer = new MidiSoundPlayer(musicFactory);
+        soundPlayer.playPitch("D4");
+        soundPlayer.playPitch("A4");
+        soundPlayer.playPitch("A4");
+        soundPlayer.playPitch("A4");
+        soundPlayer.playPitch("A4");
+        soundPlayer.playPitch("G4");
+        soundPlayer.playPitch("F#4");
+        soundPlayer.playPitch("G4");
+        soundPlayer.playPitch("F#4");
+        soundPlayer.playPitch("D4");
+        soundPlayer.playPitch("D4");
     }
 }
