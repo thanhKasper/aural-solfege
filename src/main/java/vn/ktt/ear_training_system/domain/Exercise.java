a6f9b24a0190e55c2459ea4ad77c29ff8c2cc5c8
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-13 17:31:44 +0700

    feat: include some business logics into Exercise

--- src/main/java/vn/ktt/ear_training_system/domain/Exercise.java
+++ src/main/java/vn/ktt/ear_training_system/domain/Exercise.java
@@ -1,11 +1,58 @@
 package vn.ktt.ear_training_system.domain;
 
 public abstract class Exercise {
-    private TrainingMethodology trainingMethodology;
-    private ExerciseFormat exerciseFormat;
+    private final TrainingMethodology trainingMethodology;
+    private final ExerciseFormat exerciseFormat;
+    private String title;
+    private String description;
 
     public Exercise(TrainingMethodology trainingMethodology, ExerciseFormat exerciseFormat) {
+        validateExerciseFormat(exerciseFormat);
         this.exerciseFormat = exerciseFormat;
         this.trainingMethodology = trainingMethodology;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void updateTitle(String title) {
+        validateTitle(title);
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void updateDescription(String description) {
+        this.description = description;
+    }
+
+    public ExerciseFormat getExerciseFormat() {
+        return this.exerciseFormat;
+    }
+
+    private void validateTitle(String title) {
+        if (title.isBlank()) {
+            throw new IllegalArgumentException("Invalid title, cannot be null or empty");
+        } else if (title.length() > 256) {
+            throw new IllegalArgumentException("Title can have maximum 256 characters");
+        }
+    }
+
+    private void validateDescription(String description) {
+        if (description.length() > 3000) {
+            throw new IllegalArgumentException("Description can have maximum 3000 characters");
+        }
+    }
+
+    private void validateExerciseFormat(ExerciseFormat exerciseFormat) {
+        if (exerciseFormat == null) {
+            throw new IllegalArgumentException("Exercise must have one exercise format");
+        }
+        else if (this.trainingMethodology != exerciseFormat.getTrainingMethodology()) {
+            throw new IllegalArgumentException("This exercise format cannot assign to any exercises belong to " + this.trainingMethodology.toString() + " methodology.");
+        }
     }
 }
