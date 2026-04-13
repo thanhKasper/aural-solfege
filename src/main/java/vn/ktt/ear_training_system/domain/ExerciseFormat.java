8c07ce7d4543da1b2cae1aae01a04e59ffd2e08b
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-13 17:32:13 +0700

    feat: add training methodology into ExerciseFormat

--- src/main/java/vn/ktt/ear_training_system/domain/ExerciseFormat.java
+++ src/main/java/vn/ktt/ear_training_system/domain/ExerciseFormat.java
@@ -1,9 +1,13 @@
 package vn.ktt.ear_training_system.domain;
 
 public class ExerciseFormat {
-    private TrainingMethodology trainingMethodology;
+    private final TrainingMethodology trainingMethodology;
 
     public ExerciseFormat(TrainingMethodology trainingMethodology) {
         this.trainingMethodology = trainingMethodology;
+    }
+
+    public TrainingMethodology getTrainingMethodology() {
+        return trainingMethodology;
     }
 }
