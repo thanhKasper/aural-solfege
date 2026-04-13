e23c4b60ece0ffc1f760ab0c91a31338c1387cf8
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-13 22:55:45 +0700

    feat: add validation for root aggregate

--- src/main/java/vn/ktt/ear_training_system/domain/Exercise.java
+++ src/main/java/vn/ktt/ear_training_system/domain/Exercise.java
@@ -1,15 +1,20 @@
 package vn.ktt.ear_training_system.domain;
 
-public abstract class Exercise {
+public class Exercise {
     private final TrainingMethodology trainingMethodology;
     private final ExerciseFormat exerciseFormat;
     private String title;
     private String description;
 
-    public Exercise(TrainingMethodology trainingMethodology, ExerciseFormat exerciseFormat) {
-        validateExerciseFormat(exerciseFormat);
-        this.exerciseFormat = exerciseFormat;
+    public Exercise(String title, String description, TrainingMethodology trainingMethodology, ExerciseFormat exerciseFormat) {
+        validateTitle(title);
+        validateDescription(description);
+        validateTrainingMethod(trainingMethodology);
         this.trainingMethodology = trainingMethodology;
+        validateExerciseFormatReference(exerciseFormat);
+        this.title = title;
+        this.description = description;
+        this.exerciseFormat = exerciseFormat;
     }
 
     public String getTitle() {
@@ -48,11 +53,14 @@ public abstract class Exercise {
         }
     }
 
-    private void validateExerciseFormat(ExerciseFormat exerciseFormat) {
+    private void validateTrainingMethod(TrainingMethodology method) {
+        if (method == null) throw new IllegalArgumentException("Method must not be null");
+    }
+
+    private void validateExerciseFormatReference(ExerciseFormat exerciseFormat) {
         if (exerciseFormat == null) {
             throw new IllegalArgumentException("Exercise must have one exercise format");
-        }
-        else if (this.trainingMethodology != exerciseFormat.getTrainingMethodology()) {
+        } else if (this.trainingMethodology != exerciseFormat.getTrainingMethodology()) {
             throw new IllegalArgumentException("This exercise format cannot assign to any exercises belong to " + this.trainingMethodology.toString() + " methodology.");
         }
     }
