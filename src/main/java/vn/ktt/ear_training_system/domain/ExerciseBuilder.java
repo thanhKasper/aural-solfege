33a98f4517146b80382aabc851c4656e629f0624
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-16 09:30:37 +0700

    feat: create the exercise builder

---
+++ src/main/java/vn/ktt/ear_training_system/domain/ExerciseBuilder.java
@@ -0,0 +1,26 @@
+package vn.ktt.ear_training_system.domain;
+
+import vn.ktt.ear_training_system.domain.interval_training.PassiveExerciseFormat;
+
+import java.util.List;
+
+public class ExerciseBuilder {
+    public ExerciseFormat buildPassiveExerciseFormat(String trainingMethod,
+                                                            String interval,
+                                                            String intervalProperty) {
+        return new PassiveExerciseFormat(
+                TrainingMethodology.valueOf(trainingMethod),
+                PassiveExerciseFormat.IntervalSoundProperty.valueOf(intervalProperty),
+                PassiveExerciseFormat.MusicalInterval.valueOf(interval));
+    }
+
+    public Exercise buildExercise(
+            String trainingMethod,
+            String title,
+            String description,
+            Integer reps,
+            List<ExerciseFormat> exerciseFormats
+    ) {
+        return new Exercise("", TrainingMethodology.valueOf(trainingMethod), title, description, reps, exerciseFormats);
+    }
+}
