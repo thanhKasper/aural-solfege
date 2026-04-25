bca0ec92c2c10e0a7067403a28e7ed1e44341f93
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-25 09:34:14 +0700

    feat: relocate file

--- src/main/java/vn/ktt/musical_components_core/musical_application/instrument_management/inbound/ISoundConfiguration.java
+++ src/main/java/vn/ktt/musical_components_core/musical_application/instrument_management/inbound/ISoundConfiguration.java
@@ -1,4 +1,4 @@
-package vn.ktt.musical_components_core.musical_application.sound_controller.inbound;
+package vn.ktt.musical_components_core.musical_application.instrument_management.inbound;
 
 public interface ISoundConfiguration {
     void changeInstrument(MusicalInstrument musicalInstrument);
