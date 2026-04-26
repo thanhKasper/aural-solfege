3a0841fe9bc990a64d83b640bffe2958ee3a2b47
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-26 09:14:27 +0700

    feat: add some initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,22 @@
-INSERT INTO training_methodology values (
-                                         1,
-                                         'INTERVAL_TRAINING'
-                                        );
-INSERT INTO training_methodology values (
-                                         2,
-                                         'FUNCTIONAL_EAR_TRAINING'
-                                        );
+INSERT INTO training_methodology
+values (1,
+        'INTERVAL_TRAINING');
+
+SET @target_id = random_uuid();
+
+INSERT INTO training_methodology
+values (2,
+        'FUNCTIONAL_EAR_TRAINING');
+
+INSERT INTO exercise
+values (@target_id,
+        'Practice interval perfect 5th',
+        null,
+        1,
+        2);
+
+INSERT INTO passive_training
+values (random_uuid(),
+        'P5',
+        'ASCENDING',
+        @target_id);
