59021d45131a457b1c1c286c127860f69dd22755
Kiều Tiến Thành <ktt@netcompany.com>
2026-04-28 19:21:40 +0700

    feat: create ActiveExerciseFormat

--- src/main/java/vn/ktt/ear_training_system/domain/interval_training/ActiveExerciseFormat.java
+++ src/main/java/vn/ktt/ear_training_system/domain/interval_training/ActiveExerciseFormat.java
@@ -1,4 +1,33 @@
 package vn.ktt.ear_training_system.domain.interval_training;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public abstract class ActiveExerciseFormat {
+    private final Set<MusicalInterval> practiceIntervals;
+    private final int exerciseAmounts;
+    private static final int PRACTICE_PER_INTERVAL = 5;
+
+    public ActiveExerciseFormat(List<MusicalInterval> intervals) {
+        validateSelectedIntervals(intervals);
+        this.practiceIntervals = new HashSet<>(intervals);
+        exerciseAmounts = this.practiceIntervals.size() * PRACTICE_PER_INTERVAL;
+    }
+
+    public Set<MusicalInterval> getPracticeIntervals() {
+        return practiceIntervals;
+    }
+
+    public int getExerciseAmounts() {
+        return exerciseAmounts;
+    }
+
+    public void validateSelectedIntervals(List<MusicalInterval> intervals) {
+        if (intervals.isEmpty()) {
+            throw new IllegalArgumentException("Interval must be selected");
+        } else if (intervals.size() < 2) {
+            throw new IllegalArgumentException("Choose at least two intervals to continue");
+        }
+    }
 }
