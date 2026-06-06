b8dc4ea99c6a4f3d3c4d39f5caa9911c1dad3cac
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-06-06 23:38:08 +0700

    refactor: relocate ExerciseFormatMixin to converter package and update annotations

---
+++ src/main/java/vn/ktt/ear_training_system/infrastructure/repository/converter/ExerciseFormatMixin.java
@@ -0,0 +1,12 @@
+package vn.ktt.ear_training_system.infrastructure.repository.converter;
+
+import com.fasterxml.jackson.annotation.*;
+import vn.ktt.ear_training_system.domain.interval_training.SingleIntervalExerciseFormat;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "formatType")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = SingleIntervalExerciseFormat.class, name = "single_interval")
+})
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract class ExerciseFormatMixin {
+}
