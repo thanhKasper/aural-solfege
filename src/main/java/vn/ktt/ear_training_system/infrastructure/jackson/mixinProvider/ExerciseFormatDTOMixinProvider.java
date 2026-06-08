4a901c9877deaf6bf671058c33756d9574b550a1
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-06-08 23:22:55 +0700

    Remove unused DTO provider infrastructure replaced by ExerciseFormatDTOProvider
    ExerciseFormatDTOMixinProvider, SingleIntervalExerciseFormatDTOMixinProvider,
    and ExerciseFormatDTOSubtypeRegistrar are no longer referenced after the
    migration to the simplified ExerciseFormatDTOProvider pattern.

--- src/main/java/vn/ktt/ear_training_system/infrastructure/jackson/mixinProvider/ExerciseFormatDTOMixinProvider.java
+++
@@ -1,9 +0,0 @@
-package vn.ktt.ear_training_system.infrastructure.jackson.mixinProvider;
-
-import vn.ktt.ear_training_system.application.dtos.ExerciseFormatDTO;
-
-public interface ExerciseFormatDTOMixinProvider {
-    Class<? extends ExerciseFormatDTO> targetClass();
-    Class<?> mixInClass();
-    String typeName();
-}
