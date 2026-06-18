276377a88da0610d5db1f2aefa15fbdb0f34f17b
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-06-18 22:32:18 +0700

    feat: Create ExerciseModificationGuard to enforce cross-aggregate invariants

---
+++ src/main/java/vn/ktt/ear_training_system/domain/guard/ExerciseModificationGuard.java
@@ -0,0 +1,28 @@
+package vn.ktt.ear_training_system.domain.guard;
+
+import vn.ktt.ear_training_system.domain.Exercise;
+import vn.ktt.ear_training_system.domain.practice_session.repository.IPracticeSessionRepository;
+
+public class ExerciseModificationGuard {
+    private final IPracticeSessionRepository sessionRepository;
+
+    public ExerciseModificationGuard(IPracticeSessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+
+    public void assertModifiable(Exercise exercise) {
+        if (sessionRepository.existsActiveSessionForExercise(exercise.getExerciseId())) {
+            throw new IllegalStateException(
+                    "Cannot modify exercise " + exercise.getExerciseId()
+                            + " while active practice sessions exist");
+        }
+    }
+
+    public void assertNoActiveSession(Exercise exercise) {
+        if (sessionRepository.existsActiveSessionForExercise(exercise.getExerciseId())) {
+            throw new IllegalStateException(
+                    "Exercise " + exercise.getExerciseId()
+                            + " already has an active practice session");
+        }
+    }
+}
