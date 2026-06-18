f7d7d95dd0248a2cca2b08d0bec0d8d04b768922
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-06-18 22:32:00 +0700

    feat: Create PracticeStep child entity

---
+++ src/main/java/vn/ktt/ear_training_system/domain/practice_session/entity/PracticeStep.java
@@ -0,0 +1,43 @@
+package vn.ktt.ear_training_system.domain.practice_session.entity;
+
+import lombok.Getter;
+import vn.ktt.ear_training_system.domain.practice_session.value_object.StepContext;
+import vn.ktt.ear_training_system.domain.practice_session.value_object.StepStatus;
+import vn.ktt.ear_training_system.domain.practice_session.value_object.StepType;
+
+@Getter
+public class PracticeStep {
+    private final int stepNumber;
+    private final int activityPosition;
+    private final StepType stepType;
+    private StepStatus status;
+    private final StepContext context;
+
+    PracticeStep(int stepNumber, int activityPosition, StepType stepType, StepContext context) {
+        this.stepNumber = stepNumber;
+        this.activityPosition = activityPosition;
+        this.stepType = stepType;
+        this.context = context;
+        this.status = StepStatus.PENDING;
+    }
+
+    public void activate() {
+        this.status = StepStatus.ACTIVE;
+    }
+
+    public void markCompleted() {
+        if (this.status != StepStatus.ACTIVE) {
+            throw new IllegalStateException(
+                    "Only ACTIVE steps can be completed (current: " + this.status + ")");
+        }
+        this.status = StepStatus.COMPLETED;
+    }
+
+    public void skip() {
+        if (this.status != StepStatus.ACTIVE) {
+            throw new IllegalStateException(
+                    "Only ACTIVE steps can be skipped (current: " + this.status + ")");
+        }
+        this.status = StepStatus.SKIPPED;
+    }
+}
