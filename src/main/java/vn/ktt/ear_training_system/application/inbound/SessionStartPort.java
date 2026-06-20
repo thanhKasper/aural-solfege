e07541aaa6beb4b801fff0e0952cf5a453a03c95
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-06-20 17:39:25 +0700

    refactor: Return active PracticeStepDTO from startSession instead of PracticeSession

--- src/main/java/vn/ktt/ear_training_system/application/inbound/SessionStartPort.java
+++ src/main/java/vn/ktt/ear_training_system/application/inbound/SessionStartPort.java
@@ -1,9 +1,9 @@
 package vn.ktt.ear_training_system.application.inbound;
 
-import vn.ktt.ear_training_system.domain.practice_session.entity.PracticeSession;
+import vn.ktt.ear_training_system.application.dtos.PracticeStepDTO;
 
 import java.util.UUID;
 
 public interface SessionStartPort {
-    PracticeSession startSession(UUID exerciseId);
+    PracticeStepDTO startSession(UUID exerciseId);
 }
