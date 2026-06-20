db3b476465b93b353afdd33915dfe9a59c626035
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-06-20 11:04:33 +0700

    refactor: Convert SessionResult to Java record

--- src/main/java/vn/ktt/ear_training_system/domain/practice_session/value_object/SessionResult.java
+++ src/main/java/vn/ktt/ear_training_system/domain/practice_session/value_object/SessionResult.java
@@ -1,18 +1,9 @@
 package vn.ktt.ear_training_system.domain.practice_session.value_object;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-
 import java.time.Duration;
 import java.util.Objects;
 
-@Getter
-@EqualsAndHashCode
-public class SessionResult {
-    private final int totalSteps;
-    private final int completedSteps;
-    private final Duration duration;
-
+public record SessionResult(int totalSteps, int completedSteps, Duration duration) {
     public SessionResult(int totalSteps, int completedSteps, Duration duration) {
         if (totalSteps < 1) {
             throw new IllegalArgumentException("totalSteps must be at least 1 (was " + totalSteps + ")");
