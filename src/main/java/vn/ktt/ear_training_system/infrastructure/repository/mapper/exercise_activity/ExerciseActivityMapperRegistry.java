45f7afa04497b021f75e3ada43a5bcaa5f406591
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-07-02 23:22:45 +0700

    feat: add IExerciseActivityMapper interface and registry for polymorphic dispatch

---
+++ src/main/java/vn/ktt/ear_training_system/infrastructure/repository/mapper/exercise_activity/ExerciseActivityMapperRegistry.java
@@ -0,0 +1,33 @@
+package vn.ktt.ear_training_system.infrastructure.repository.mapper.exercise_activity;
+
+import org.springframework.stereotype.Component;
+import vn.ktt.ear_training_system.domain.exercise.entity.ExerciseActivity;
+import vn.ktt.ear_training_system.infrastructure.repository.entities.exercise_activities.ExerciseActivityEntity;
+import vn.ktt.shared.DataMapperRegistry;
+
+import java.util.List;
+
+@Component
+public class ExerciseActivityMapperRegistry extends DataMapperRegistry<ExerciseActivity, ExerciseActivityEntity, IExerciseActivityMapper> {
+    public ExerciseActivityMapperRegistry(List<IExerciseActivityMapper> exerciseActivityMappers) {
+        super(exerciseActivityMappers);
+    }
+
+    public ExerciseActivity toExerciseActivity(ExerciseActivityEntity entity) {
+        var mapper = getMapperBaseOnDataTo(entity);
+        if (mapper != null) {
+            return mapper.reverseTransform(entity);
+        }
+
+        throw new IllegalArgumentException("Missing mapper to convert entity " + entity.getClass());
+    }
+
+    public ExerciseActivityEntity toExerciseActivityEntity(ExerciseActivity domain) {
+        var mapper = getMapperBaseOnDataFrom(domain);
+        if (mapper != null) {
+            return mapper.transform(domain);
+        }
+
+        throw new IllegalArgumentException("Missing mapper to convert entity " + domain.getClass());
+    }
+}
---
+++ src/main/java/vn/ktt/ear_training_system/infrastructure/repository/mapper/exercise_activity/IExerciseActivityMapper.java
@@ -0,0 +1,8 @@
+package vn.ktt.ear_training_system.infrastructure.repository.mapper.exercise_activity;
+
+import vn.ktt.ear_training_system.domain.exercise.entity.ExerciseActivity;
+import vn.ktt.ear_training_system.infrastructure.repository.entities.exercise_activities.ExerciseActivityEntity;
+import vn.ktt.shared.IDataMapper;
+
+public interface IExerciseActivityMapper extends IDataMapper<ExerciseActivity, ExerciseActivityEntity> {
+}
