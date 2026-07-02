ed257becfc8bc22dfbd3c63c303a1f81e50117e7
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-07-02 23:22:56 +0700

    refactor: remove obsolete exercise activity mixin infrastructure

--- src/main/java/vn/ktt/ear_training_system/infrastructure/jackson/mixin/entity/exercise_activity/ExerciseActivityMixin.java
+++
@@ -1,8 +0,0 @@
-package vn.ktt.ear_training_system.infrastructure.jackson.mixin.entity.exercise_activity;
-
-import com.fasterxml.jackson.annotation.*;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-@JsonIgnoreProperties(ignoreUnknown = true)
-public abstract class ExerciseActivityMixin {
-}
--- src/main/java/vn/ktt/ear_training_system/infrastructure/jackson/mixin_provider/entity/exercise_activity/ExerciseActivityMixInProvider.java
+++
@@ -1,9 +0,0 @@
-package vn.ktt.ear_training_system.infrastructure.jackson.mixin_provider.entity.exercise_activity;
-
-import vn.ktt.ear_training_system.domain.exercise.entity.ExerciseActivity;
-
-public interface ExerciseActivityMixInProvider {
-    Class<? extends ExerciseActivity> targetClass();
-    Class<?> mixInClass();
-    String typeName();
-}
