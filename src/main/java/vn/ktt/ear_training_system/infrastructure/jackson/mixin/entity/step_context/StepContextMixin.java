020ce1d66851aaf1518be7d764acd36d7012598a
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-07-04 15:02:31 +0700

    chore: remove unused StepContext Jackson mixin infrastructure
    The @JsonTypeInfo annotations are now directly on the StepContextEntity
    hierarchy, making the separate mixin classes redundant

--- src/main/java/vn/ktt/ear_training_system/infrastructure/jackson/mixin/entity/step_context/StepContextMixin.java
+++
@@ -1,7 +0,0 @@
-package vn.ktt.ear_training_system.infrastructure.jackson.mixin.entity.step_context;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-public class StepContextMixin {
-}
--- src/main/java/vn/ktt/ear_training_system/infrastructure/jackson/mixin_provider/entity/step_context/StepContextMixinProvider.java
+++
@@ -1,9 +0,0 @@
-package vn.ktt.ear_training_system.infrastructure.jackson.mixin_provider.entity.step_context;
-
-import vn.ktt.ear_training_system.domain.practice_session.value_object.StepContext;
-
-public interface StepContextMixinProvider {
-    Class<? extends StepContext> targetClass();
-    Class<?> mixInClass();
-    String typeName();
-}
