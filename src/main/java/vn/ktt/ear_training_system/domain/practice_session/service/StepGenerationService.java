454a8fc0a237365d1e71d6b046b552cad7fee9f2
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-07-12 22:53:35 +0700

    feat: round-robin repetition of activity step passes

--- src/main/java/vn/ktt/ear_training_system/domain/practice_session/service/StepGenerationService.java
+++ src/main/java/vn/ktt/ear_training_system/domain/practice_session/service/StepGenerationService.java
@@ -5,6 +5,7 @@ import vn.ktt.ear_training_system.domain.practice_session.value_object.StepDefin
 import vn.ktt.shared.IServiceIndex;
 import vn.ktt.shared.ServiceRegistry;
 
+import java.util.Collections;
 import java.util.List;
 
 public class StepGenerationService extends ServiceRegistry<ExerciseActivity, StepGeneration> {
@@ -13,8 +14,8 @@ public class StepGenerationService extends ServiceRegistry<ExerciseActivity, Ste
         super(generatorList);
     }
 
-    public List<StepDefinition> generate(List<ExerciseActivity> activities) {
-        return activities.stream()
+    public List<StepDefinition> generate(List<ExerciseActivity> activities, int repetition) {
+        var onePass = activities.stream()
                 .flatMap(activity -> {
                     var generator = this.getService(activity);
                     if (generator == null) {
@@ -23,6 +24,10 @@ public class StepGenerationService extends ServiceRegistry<ExerciseActivity, Ste
                     }
                     return generator.generate(activity).stream();
                 })
+                .toList();
+
+        return Collections.nCopies(repetition, onePass).stream()
+                .flatMap(List::stream)
                 .toList();
     }
 }
