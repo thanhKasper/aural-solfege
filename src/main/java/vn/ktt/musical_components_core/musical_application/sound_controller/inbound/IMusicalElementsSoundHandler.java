f1f6c904832515dd7c31ee5b466740a2c668e2e6
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-08-01 19:37:46 +0700

    refactor: remove sound handler flow

--- src/main/java/vn/ktt/musical_components_core/musical_application/sound_controller/inbound/IMusicalElementsSoundHandler.java
+++
@@ -1,9 +0,0 @@
-package vn.ktt.musical_components_core.musical_application.sound_controller.inbound;
-
-public interface IMusicalElementsSoundHandler {
-    void playStackedInterval(String interval, String pitch);
-    void playBrokenInterval(String interval, String anchoredPitch, boolean isUpward);
-    void playAscendingSequentially(String interval);
-    void playDescendingSequentially(String interval);
-    void playStackSequentially(String interval);
-}
