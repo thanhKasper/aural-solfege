971456113ad4d34c32659c57cd212a868cfcd4ea
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-08-08 23:19:42 +0700

    refactor: enhance the Instrument.java domain entity

--- src/main/java/vn/ktt/musical_components_core/musical_domains/instruments/Instrument.java
+++ src/main/java/vn/ktt/musical_components_core/musical_domains/instruments/Instrument.java
@@ -1,9 +1,23 @@
 package vn.ktt.musical_components_core.musical_domains.instruments;
 
+import lombok.Getter;
 import vn.ktt.musical_components_core.musical_domains.music_atom.Pitch;
+import vn.ktt.musical_components_core.musical_domains.music_factory.IMusicalEntityFactory;
 
-public interface Instrument {
-    InstrumentType getInstrumentType();
-    Pitch getLowestPitch();
-    Pitch getHighestPitch();
+@Getter
+public class Instrument {
+    private final InstrumentType instrumentType;
+    private final Pitch lowestPitch;
+    private final Pitch highestPitch;
+
+    // Not allow other class to use default initialization
+    private Instrument(InstrumentType instrumentType, Pitch lowestPitch, Pitch highestPitch) {
+        this.instrumentType = instrumentType;
+        this.lowestPitch = lowestPitch;
+        this.highestPitch = highestPitch;
+    }
+
+    public static Instrument reconstruct(IMusicalEntityFactory musicalEntityFactory, InstrumentType instrumentType, String lowestPitch, String highestPitch) {
+        return new Instrument(instrumentType, musicalEntityFactory.getPitch(lowestPitch), musicalEntityFactory.getPitch(highestPitch));
+    }
 }
