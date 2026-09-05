2198dac2cb48276b5cba5749f7d40ca7a579044d
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-09-05 13:20:38 +0700

    feat: add one more exercise that has intervals sound comparison for testing

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
 INSERT INTO exercises (id, training_methodology, title, description, repetitions, rest, exercise_activities) VALUES ('48cdbd72-83ca-4be4-8292-6fc1db16b10a', 'INTERVAL_TRAINING', 'Basic Interval Training', 'Practice identifying major 2nd and perfect 5th intervals in ascending motion.', 2, 100, '[{"type": "SINGLE_INTERVAL", "soundProperty": "ASCENDING", "intervals": ["MAJOR_2ND"], "position": 0}, {"type": "SINGLE_INTERVAL", "soundProperty": "ASCENDING", "intervals": ["PERFECT_5TH"], "position": 1}, {"type": "COOL_DOWN", "position": 2, "restAmountInSecond": 100}]');
 
+INSERT INTO exercises (id, training_methodology, title, description, repetitions, rest, exercise_activities) VALUES ('2f3ae143-610c-474c-bb59-db4b4d3c08ba', 'INTERVAL_TRAINING', 'Interval Sound Comparison', 'Practice comparing whether the second interval is higher or lower than the first interval.', 2, 100, '[{"type": "INTERVAL_SOUND_COMPARISON", "texture": "ASCENDING", "firstInterval": "PERFECT_4TH", "secondInterval": "PERFECT_5TH", "position": 0}, {"type": "COOL_DOWN", "position": 1, "restAmountInSecond": 100}]');
+
 INSERT INTO instruments (id, instrument_type, lowest_pitch, highest_pitch) values ('61457859-e70c-4a5f-b041-ae4e7171c6ab', 'PIANO', 'A0', 'C8');
 
 INSERT INTO musical_config (id, active_instrument_id) SELECT gen_random_uuid(), i.id FROM instruments i WHERE i.instrument_type = 'PIANO';
