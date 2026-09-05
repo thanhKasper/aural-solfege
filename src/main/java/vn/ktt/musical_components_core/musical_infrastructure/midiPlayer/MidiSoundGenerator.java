44a87e772ea2c523f355480fa1b0370c7962bfd7
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-09-05 21:26:26 +0700

    feat: add a comment for future enhancement

--- src/main/java/vn/ktt/musical_components_core/musical_infrastructure/midiPlayer/MidiSoundGenerator.java
+++ src/main/java/vn/ktt/musical_components_core/musical_infrastructure/midiPlayer/MidiSoundGenerator.java
@@ -12,7 +12,7 @@ import vn.ktt.musical_components_core.musical_infrastructure.midiPlayer.renderer
 
 import javax.sound.midi.Sequence;
 
-// @TODO: Not precise base on how the sound is processed, need more investigation to create a more completed audio file generation.
+// @TODO: Not precise base on how the sound is processed, need more investigation to create a more completed audio file generation. Need an architecture redesign
 @Component
 public class MidiSoundGenerator implements SoundGeneratorPort {
 
